41520e9fb4cf869542df4cedd9aa625a35f3d080
hassain-k <hhassaink@gmail.com>
2025-05-07 15:09:14 +0530

    odd or even problem

--- 4.oddoreven.c
+++ 4.oddoreven.c
@@ -1,1 +1,14 @@
-tt
+#include<stdio.h>
+int main()
+{
+
+    int a;
+    printf("Enter a Number:");
+    scanf("%d",&a);
+
+    if(a%2==0)
+    printf("%d : It is Even Number",a);
+
+    else 
+    printf("%d : It is odd Number",a);
+}
