97737ba7ea509a9f3cef0678f3a285b3bfa33d67
hassain-k <hhassaink@gmail.com>
2025-05-08 04:55:47 +0530

    Absolute value program

---
+++ 6.absolutevalue.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter Number:");
+    scanf("%d",&a);
+    if(a>0)
+    printf("Value is :%d",a);
+
+    else printf("Value is :%d",(-1*a));
+   
+}
